f83bae6750253e30497ae2652debff5adf86b81f
Stefan Kalscheuer <stefan@stklcode.de>
2020-10-18 14:49:50 +0200

    rework unit tests using WP_Mock
    Split tests into separate classes for the plugin itself and the modules.
    Mock WP environment instead of installing it.
    Update PHPUnit to 7.x again and use PHP 7.1+ only for CI.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,22 +1,13 @@
 <?php
-
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
-
-require_once $_tests_dir . '/includes/functions.php';
-
 /**
- * Load the plugin.
+ * PHPUnit bootstrapping.
+ *
+ * @package AntiVirus
  */
-function _manually_load_oembed_api_plugin() {
-	require dirname( __FILE__ ) . '/../antivirus.php';
-}
 
-tests_add_filter( 'muplugins_loaded', '_manually_load_oembed_api_plugin' );
+require_once __DIR__ . '/../vendor/autoload.php';
 
-require $_tests_dir . '/includes/bootstrap.php';
+WP_Mock::bootstrap();
 
-// Include custom mocks.
-require 'wp_mocks.php';
+require_once __DIR__ . '/antivirustestcase.php';
+require_once __DIR__ . '/../inc/class-antivirus.php';
