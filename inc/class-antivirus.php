dae30c2b52fda4271dfa7cb2b90b9609aff9623b
Stefan Kalscheuer <stefan@stklcode.de>
2021-05-13 15:02:59 +0200

    open transparency report link in new window

--- inc/class-antivirus.php
+++ inc/class-antivirus.php
@@ -729,13 +729,19 @@ class AntiVirus {
 											/* translators: First placeholder (%1$s) starting link tag to transparency report, second placeholder (%2$s) closing link tag */
 											__( 'For more details read %1$sthe transparency report%2$s.', 'antivirus' ),
 											sprintf(
-												'<a href="https://transparencyreport.google.com/safe-browsing/search?url=%s&hl=%s">',
+												'<a href="https://transparencyreport.google.com/safe-browsing/search?url=%s&hl=%s" target="_blank" rel="noopener noreferrer">',
 												urlencode( get_bloginfo( 'url' ) ),
 												substr( get_locale(), 0, 2 )
 											),
 											'</a>'
 										),
-										array( 'a' => array( 'href' => array() ) )
+										array(
+											'a' => array(
+												'href'   => array(),
+												'target' => array(),
+												'rel'    => array(),
+											),
+										)
 									);
 									?>
 								</p>
