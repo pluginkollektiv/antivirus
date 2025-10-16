pluginkollektiv/antivirus

commit 819272cef9476cc1eeb66bfcaa744f5e07a4adf3

rewrite JavaScript and drop jQuery dependency (#157)
Rewrite the script in plain ES2015, drop jQuery and split methods to
make the code more maintainable.

js/script.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(($) => {
+document.addEventListener('DOMContentLoaded', () => {
 	// Initialize.
 	const avNonce = av_settings.nonce;
 	let avFiles = [];
@@ -9,38 +9,36 @@ jQuery(document).ready(($) => {
 	 *
 	 * @param {number} current File index to scan.
 	 */
-	function checkThemeFile(current) {
+	function checkThemeFile(current = 0) {
 		// Sanitize ID.
-		const id = parseInt(current || 0);
+		const id = parseInt(current);
 
 		// Get corresponding file.
 		const file = avFiles[id];
 
 		// Issue the request.
-		$.post(
-			ajaxurl,
-			{
-				action: 'get_ajax_response',
-				_ajax_nonce: avNonce,
-				_theme_file: file,
-				_action_request: 'check_theme_file',
-			},
-			(input) => {
+		ajaxRequest({
+			_theme_file: file,
+			_action_request: 'check_theme_file',
+		})
+			.then((res) => res.text())
+			.then((input) => {
 				// Data present?
 				if (input) {
+					input = JSON.parse(input);
 					// Verify nonce.
 					if (!input.nonce || input.nonce !== avNonce) {
 						return;
 					}
 
 					// Set highlighting color.
-					const row = $('#av-scan-result-' + id);
-					row.addClass('av-status-warning').removeClass(
-						'av-status-pending'
+					const row = document.getElementById(`av-scan-result-${id}`);
+					row.classList.replace(
+						'av-status-pending',
+						'av-status-warning'
 					);
-					row.find('td.av-status-column').text(
-						wp.i18n.__('! Warning', 'antivirus')
-					);
+					row.querySelector('td.av-status-column').innerText =
+						wp.i18n.__('! Warning', 'antivirus');
 
 					// Initialize lines of current file.
 					const lines = input.data;
@@ -52,75 +50,21 @@ jQuery(document).ready(($) => {
 							.replace(/@span@/g, '<span>')
 							.replace(/@\/span@/g, '</span>');
 
-						row.find('td.av-file-column').append(
-							'<p><code>' +
-								line +
-								'</code> <a href="#" id="av-dismiss-' +
-								md5 +
-								'" class="button" title="' +
-								wp.i18n.__(
-									'Dismiss false positive virus detection',
-									'antivirus'
-								) +
-								'">' +
-								wp.i18n.__('Dismiss', 'antivirus') +
-								'</a></p>'
-						);
+						row.querySelector('td.av-file-column').innerHTML +=
+							`<p><code>${line}</code> <a href="#" id="av-dismiss-${md5}" class="button" ` +
+							`title="${wp.i18n.__('Dismiss false positive virus detection', 'antivirus')}">` +
+							wp.i18n.__('Dismiss', 'antivirus') +
+							'</a></p>';
 
-						$('#av-dismiss-' + md5).click((evt) => {
-							$.post(
-								ajaxurl,
-								{
-									action: 'get_ajax_response',
-									_ajax_nonce: avNonce,
-									_file_md5: evt.target.id.substring(11),
-									_action_request: 'update_white_list',
-								},
-								(res) => {
-									// No data received?
-									if (!res) {
-										return;
-									}
-
-									// Security check.
-									if (!res.nonce || res.nonce !== avNonce) {
-										return;
-									}
-
-									// Get table column above the dismiss button.
-									const issue = $(
-										'#av-dismiss-' + res.data[0]
-									).parent();
-									const col = issue.parent();
-
-									// Hide code details and "dismiss" button.
-									issue.hide('slow').remove();
-
-									// Mark row as "OK", if no more issues are present.
-									if (col.find('p').length === 0) {
-										col.parent()
-											.addClass('av-status-ok')
-											.removeClass('av-status-warning');
-										col.parent()
-											.find('td.av-status-column')
-											.text(
-												wp.i18n.__('✔ OK', 'antivirus')
-											);
-									}
-								}
-							);
-
-							return false;
-						});
+						document
+							.getElementById(`av-dismiss-${md5}`)
+							?.addEventListener('click', handleDismiss);
 					}
 				} else {
-					const row = $('#av-scan-result-' + id);
-					row.addClass('av-status-ok').removeClass(
-						'av-status-pending'
-					);
-					row.find('td.av-status-column').text(
-						wp.i18n.__('✔ OK', 'antivirus')
-					);
+					const row = document.getElementById(`av-scan-result-${id}`);
+					row.classList.add('av-status-pending', 'av-status-ok');
+					row.querySelector('td.av-status-column').innerText =
+						wp.i18n.__('✔ OK', 'antivirus');
 				}
 
 				// Increment counter.
@@ -128,51 +72,70 @@ jQuery(document).ready(($) => {
 
 				// Output notification.
 				if (avFilesLoaded >= avFiles.length) {
-					$('#av-scan-process')
-						.html(
-							'<span class="av-scan-complete">' +
-								wp.i18n.__('Scan finished', 'antivirus') +
-								'</span>'
-						)
-						.fadeOut()
-						.fadeIn()
-						.fadeOut()
-						.fadeIn()
-						.fadeOut()
-						.fadeIn()
-						.animate({ opacity: 1.0 }, 500);
+					document.getElementById('av-scan-process').innerHTML =
+						'<span class="av-scan-complete">' +
+						wp.i18n.__('Scan finished', 'antivirus') +
+						'</span>';
 				} else {
 					checkThemeFile(id + 1);
 				}
-			}
-		);
+			});
 	}
 
-	// Check templates.
-	$('#av-scan-trigger').click(() => {
-		// Request.
-		$.post(
-			ajaxurl,
-			{
-				action: 'get_ajax_response',
-				_ajax_nonce: avNonce,
-				_action_request: 'get_theme_files',
-			},
-			(input) => {
-				// Initialize output value.
-				let output =
-					'<table class="wp-list-table widefat fixed striped table-view-list av-scan-results">' +
-					'<thead><tr>' +
-					'<td class="av-toggle-column check-column"></td>' +
-					'<th class="av-file-column">' +
-					wp.i18n.__('Theme File', 'antivirus') +
-					'</th>' +
-					'<th class="av-status-column">' +
-					wp.i18n.__('Check Status', 'antivirus') +
-					'</th>' +
-					'</tr></thead>' +
-					'<tbody>';
+	/**
+	 * Event handler for the "dismiss" button.
+	 * Notify the API and update the UI accordingly.
+	 *
+	 * @param {MouseEvent} evt Click event
+	 * @return {boolean} false
+	 */
+	function handleDismiss(evt) {
+		ajaxRequest({
+			_file_md5: evt.target.id.substring(11),
+			_action_request: 'update_white_list',
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				// No data received or missing nonce?
+				if (!res || !res.nonce || res.nonce !== avNonce) {
+					return;
+				}
 
+				// Get table column above the dismiss button.
+				const issue = document.getElementById(
+					`av-dismiss-${res.data[0]}`
+				)?.parentElement;
+				const col = issue?.parentElement;
+
+				// Hide code details and "dismiss" button.
+				issue?.remove();
+
+				// Mark row as "OK", if no more issues are present.
+				if (col.getElementsByTagName('p').length === 0) {
+					col.parentElement?.classList.replace(
+						'av-status-warning',
+						'av-status-ok'
+					);
+					col.parentElement.querySelector(
+						'td.av-status-column'
+					).innerText = wp.i18n.__('✔ OK', 'antivirus');
+				}
+			});
+
+		return false;
+	}
+
+	/**
+	 * Trigger a manual scan.
+	 *
+	 * @return {boolean} false
+	 */
+	function triggerScan() {
+		ajaxRequest({
+			_action_request: 'get_theme_files',
+		})
+			.then((res) => res.json())
+			.then((input) => {
 				// No data received?
 				if (!input) {
 					return;
@@ -187,86 +150,126 @@ jQuery(document).ready(($) => {
 				avFiles = input.data;
 				avFilesLoaded = 0;
 
-				// Visualize files.
-				$.each(avFiles, (i, val) => {
-					output +=
-						'<tr id="av-scan-result-' +
-						i +
-						'" class="av-status-pending">' +
-						'<td class="av-toggle-column check-column"></td>' +
-						'<td class="av-file-column">' +
-						val +
-						'</td>' +
-						'<td class="av-status-column">' +
-						wp.i18n.__('pending', 'antivirus') +
-						'</td>' +
-						'</tr>';
-				});
-
-				output +=
-					'</tbody><tfoot><tr>' +
-					'<td class="av-toggle-column check-column"></td>' +
-					'<th class="av-file-column">' +
-					wp.i18n.__('Theme File', 'antivirus') +
-					'</th><th class="av-status-column">' +
-					wp.i18n.__('Check Status', 'antivirus') +
-					'</th></tr></tfoot></table>';
-
 				// assign values.
-				$('#av-scan-process').html(
-					'<span class="spinner is-active" title="running"></span>'
-				);
-				$('#av-scan-output').empty().append(output);
+				let elem = document.getElementById('av-scan-process');
+				if (elem) {
+					elem.innerHTML =
+						'<span class="spinner is-active" title="running"></span>';
+				}
+				elem = document.getElementById('av-scan-output');
+				if (elem) {
+					elem.innerHTML = generateThemeFileTable();
+				}
 
 				// Start loop through files.
 				checkThemeFile();
-			}
-		);
-
+			});
 		return false;
-	});
+	}
+
+	/**
+	 * Generate an HTML table for theme files before scanning.
+	 *
+	 * @return {string} HTML table markup
+	 */
+	function generateThemeFileTable() {
+		// Initialize output value.
+		let output =
+			'<table class="wp-list-table widefat fixed striped table-view-list av-scan-results">' +
+			'<thead><tr>' +
+			'<td class="av-toggle-column check-column"></td>' +
+			`<th class="av-file-column">${wp.i18n.__('Theme File', 'antivirus')}</th>` +
+			`<th class="av-status-column">${wp.i18n.__('Check Status', 'antivirus')}</th>` +
+			'</tr></thead>' +
+			'<tbody>';
+
+		avFiles.forEach((val, i) => {
+			output +=
+				`<tr id="av-scan-result-${i}" class="av-status-pending">` +
+				'<td class="av-toggle-column check-column"></td>' +
+				`<td class="av-file-column">${val}</td>` +
+				`<td class="av-status-column">${wp.i18n.__('pending', 'antivirus')}</td>` +
+				'</tr>';
+		});
+
+		output +=
+			'</tbody><tfoot><tr>' +
+			'<td class="av-toggle-column check-column"></td>' +
+			`<th class="av-file-column">${wp.i18n.__('Theme File', 'antivirus')}</th>` +
+			`<th class="av-status-column">${wp.i18n.__('Check Status', 'antivirus')}</th>` +
+			'</tr></tfoot></table>';
+
+		return output;
+	}
 
 	/**
 	 * Manage dependent option inputs.
 	 */
 	function manageOptions() {
-		const cbSelectors = [
-			'#av_cronjob_enable',
-			'#av_safe_browsing',
-			'#av_checksum_verifier',
+		const cbIds = [
+			'av_cronjob_enable',
+			'av_safe_browsing',
+			'av_checksum_verifier',
 		];
 		let anyEnabled = false;
 
-		cbSelectors.forEach((c) => {
-			const cb = $(c);
-			const inputs = cb
-				.parents('fieldset')
-				.find(':text, :checkbox')
-				.not(cb);
-
-			// Disable all other inputs of current fieldset, if unchecked.
-			let enabled;
-			if (typeof $.fn.prop === 'function') {
-				enabled = !!cb.prop('checked');
-				inputs.prop('disabled', !enabled);
-			} else {
-				enabled = !!cb.attr('checked');
-				inputs.attr('disabled', !enabled);
+		for (const c of cbIds) {
+			const cb = document.getElementById(c);
+			if (!cb) {
+				continue;
 			}
 
+			const enabled = cb.checked;
+			cb?.closest('fieldset')
+				?.querySelectorAll('input[type="text"], input[type="checkbox"]')
+				.forEach((input) => {
+					if (input !== cb) {
+						input.disabled = !enabled;
+					}
+				});
+
 			anyEnabled = anyEnabled || enabled;
-		});
+		}
 
 		// Enable email notification if any module is enabled.
-		if (typeof $.fn.prop === 'function') {
-			$('#av_notify_email').prop('disabled', !anyEnabled);
-		} else {
-			$('#av_notify_email').attr('disabled', !anyEnabled);
+		const elem = document.getElementById('av_notify_email');
+		if (elem) {
+			elem.disabled = !anyEnabled;
 		}
 	}
 
+	/**
+	 * Make an AJAX request.
+	 *
+	 * @param {Record<string, string>} data Payload (action and nonce not included)
+	 * @return {Promise<Response>} response promise
+	 */
+	function ajaxRequest(data) {
+		return fetch(ajaxurl, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: new URLSearchParams({
+				action: 'get_ajax_response',
+				_ajax_nonce: avNonce,
+				...data,
+			}),
+		});
+	}
+
+	// ----- initialize DOM elements  -----
+
+	// Check templates.
+	document
+		.getElementById('av-scan-trigger')
+		?.addEventListener('click', triggerScan);
+
 	// Watch checkboxes.
-	$('#av_settings input[type=checkbox]').click(manageOptions);
+	document
+		.getElementById('av_settings')
+		?.querySelectorAll('input[type=checkbox]')
+		.forEach((cb) => cb.addEventListener('click', manageOptions));
 
 	// Handle initial checkbox values.
 	manageOptions();

inc/class-antivirus.php
@@ -324,7 +324,7 @@ class AntiVirus {
 		wp_enqueue_script(
 			'av_script',
 			plugins_url( 'js/script.min.js', ANTIVIRUS_FILE ),
-			array( 'jquery' ),
+			array(),
 			$data['Version'],
 			true
 		);

commit 5304fdabc54977954ebb579dc1a0bfc5eeac5ea6

fix: preserve event listener on multiple dismiss buttons (#161) (#162)
Appending to innerHTML makes the browser re-render the parent node which
removes the previously added event listener.
Because of this only the last "dismiss" button within a file row
actually works.

Rewrite the logic to create a node programmatically and append it, so
the element does not get reset in the loop.

Fixes: 819272cef9476cc1eeb66bfcaa744f5e07a4adf3

js/script.js
@@ -50,15 +50,21 @@ document.addEventListener('DOMContentLoaded', () => {
 							.replace(/@span@/g, '<span>')
 							.replace(/@\/span@/g, '</span>');
 
-						row.querySelector('td.av-file-column').innerHTML +=
-							`<p><code>${line}</code> <a href="#" id="av-dismiss-${md5}" class="button" ` +
-							`title="${wp.i18n.__('Dismiss false positive virus detection', 'antivirus')}">` +
-							wp.i18n.__('Dismiss', 'antivirus') +
-							'</a></p>';
-
-						document
-							.getElementById(`av-dismiss-${md5}`)
-							?.addEventListener('click', handleDismiss);
+						const p = document.createElement('p');
+						const code = document.createElement('code');
+						code.innerHTML = line;
+						const btn = document.createElement('a');
+						btn.id = `av-dismiss-${md5}`;
+						btn.href = '#';
+						btn.classList.add('button');
+						btn.title = wp.i18n.__(
+							'Dismiss false positive virus detection',
+							'antivirus'
+						);
+						btn.innerText = wp.i18n.__('Dismiss', 'antivirus');
+						btn.addEventListener('click', handleDismiss);
+						p.append(code, btn);
+						row.querySelector('td.av-file-column').append(p);
 					}
 				} else {
 					const row = document.getElementById(`av-scan-result-${id}`);